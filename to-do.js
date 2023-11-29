5061197715fc920486143b7d05cef1d3787e1ad8
Батболд Гансүх <23LP0812@G304-CC0087.local>
2023-11-29 19:43:43 +0800

    sdsd

--- to-do.js
+++ to-do.js
@@ -4,44 +4,99 @@ const addTask = document.getElementById ("addTask");
 const titleRef = document.getElementById("text");
 const descriptionRef = document.getElementById("description");
 const statusRef = document.getElementById("status");
-const priorityInputRef = document.getElementById("priority-input")
+const priorityInputRef = document.getElementById("priority")
+const task = document.getElementById("task");
+const taskContainer = document.querySelectorAll(".task");
+const inProText = document.getElementById("inProText");
+const stuckText = document.getElementById("stuck-Text");
+const doneText = document.getElementById("doneCont")
 
-console.log(btnRef , titleRef , descriptionRef);
+console.log("taskContainer", taskContainer);
 
 
 for ( var i = 0; i < btnRef.length; i++){
     btnRef[i].addEventListener("click" , () => {
         input.style.visibility = "visible"
     })
-
 }
-addTask.addEventListener ("click", () => {
-    input.style.visibility = "hidden"
-})
+
+function createTask(title, description, priority) {
+    const taskCont = document.createElement("div");
+    taskCont.id = "taskCont";
+    task.appendChild(taskCont);
+
+    const done = document.createElement("div");
+    done.id = "done";
+    done.innerHTML = `<i class="fa-solid fa-check"></i>`;
+    taskCont.appendChild(done);
+
+    const text = document.createElement("div");
+    text.id = "textCart";
+    taskCont.appendChild(text);
+
+    const action = document.createElement("div");
+    action.id = "action";
+    taskCont.appendChild(action);
+
+    // Action new elements
+    const deleteBtnRef = document.createElement("i");
+    const editBtn = document.createElement("i");
+
+    deleteBtnRef.id = "deleteBtn";
+    deleteBtnRef.innerHTML = `<i class="fa-solid fa-x"></i>`;
+
+    editBtn.id = "editBtn";
+    editBtn.innerHTML = `<i class="fa-regular fa-pen-to-square"></i>`;
+    action.appendChild(deleteBtnRef);
+    action.appendChild(editBtn);
+
+    // REMOVE BTNs
+    deleteBtnRef.addEventListener("click", (event) => {
+        console.log(event.target.parentNode)
+        const parent = event.target.parentNode.parentNode.parentNode
+        console.log(parent.parentNode);
+        const taskContainer = parent.parentNode;
+        taskContainer.removeChild(taskCont)
+   
+    
+    });
 
 
-//-------- creat elment ------------------
+    // DONE BTN
+    done.addEventListener("click", () => {
+        doneText.appendChild(taskCont);
+    });
 
-//----- ERONHII AGUULJ BUI CONTAINER
-const cart = document.createElement ("div");
+    const taskTitle = document.createElement("h1");
+    text.appendChild(taskTitle);
+    taskTitle.textContent = title;
 
-// -----------------------------------------
+    const taskDescription = document.createElement("p");
+    text.appendChild(taskDescription);
+    taskDescription.textContent = description;
 
-//              SECTION VIEW ELMENTS 
+    const taskPriority = document.createElement("p");
+    taskPriority.id = "valueS";
+    text.appendChild(taskPriority);
+    taskPriority.textContent = priority;
+}
 
-const done = document.createElement ("div");
-const textCont = document.createElement ("div");
-const actionBtn = document.createElement ("div");
+addTask.addEventListener("click", () => {
+    input.style.visibility = "hidden";
 
 
-//       TEXT CONTAINER DOTROH ELMENTS
+    // Call the createTask function with the input values
+    createTask(titleRef.value, descriptionRef.value, priorityInputRef.value);
+    titleRef.value = ""
+    descriptionRef.value = ""
+});
 
-const title = document.createElement("h1");
-const description = document.createElement ("p");
 
-//        APPEND CHILDE BY TEXT CONTAINER
 
-textCont.append (title, description)
+
+
+
+
 
 
 
